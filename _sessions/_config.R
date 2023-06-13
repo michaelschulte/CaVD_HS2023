b8a06385e0b8d94e2763468aa6e99f319752c0d7
Michael Schulte <michael@MacBookisaster2.fritz.box>
2023-06-13 08:25:37 +0200

    updates

---
+++ _sessions/_config.R
@@ -0,0 +1,10 @@
+# Site settings
+session_url = 'https://therbootcamp.github.io/Intro2R_Unibe_2022/_sessions/'
+base_url = 'https://therbootcamp.github.io/Intro2R_Unibe_2022/'
+raw_url = 'https://raw.githubusercontent.com/therbootcamp/Intro2R_Unibe_2022/'
+
+# if TRUE = show solutions in practicals
+value_echo = FALSE
+value_eval = FALSE
+
+baselink <- 'https://CaVD_HS2023.netlify.app/'
